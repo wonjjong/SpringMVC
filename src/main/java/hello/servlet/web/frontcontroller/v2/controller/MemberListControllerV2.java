eeb5fa66ea187a9768c5340ebdb6533cbea63f0c
wonjjong <wonjjong.dev@gmail.com>
2021-05-03 16:12:12 +0900

    frontController v2

--- src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberListControllerV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberListControllerV2.java
@@ -1,8 +1,9 @@
-package hello.servlet.web.frontcontroller.v1.controller;
+package hello.servlet.web.frontcontroller.v2.controller;
 
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
-import hello.servlet.web.frontcontroller.v1.ControllerV1;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v2.ControllerV2;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,17 +12,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-public class MemberListControllerV1 implements ControllerV1 {
+public class MemberListControllerV2 implements ControllerV2 {
 
     private static MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
-    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         List<Member> members = memberRepository.findAll();
         request.setAttribute("members", members);
 
-        String viewPath = "/WEB-INF/views/members.jsp";
-        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
-        dispatcher.forward(request, response);
+        return new MyView("/WEB-INF/views/members.jsp");
     }
 }
